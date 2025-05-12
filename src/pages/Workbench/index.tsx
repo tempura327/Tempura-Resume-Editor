2e354f76d27f5bf5726c50e903e37bc07cffd5f8
tempura327 <wanglt242@gmail.com>
2025-05-12 22:16:07 +0800

    feat: put a canvas to Workbench

--- src/pages/Workbench/index.tsx
+++ src/pages/Workbench/index.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useRef } from 'react';
+
+const A4_RATIO = 1.38;
+const CANVAS_WIDTH = window.innerWidth * 0.66;
+const CANVAS_HEIGHT = CANVAS_WIDTH * A4_RATIO;
+
 const Workbench = () => {
-  return <div>todo</div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d');
+  }, []);
+
+  return (
+    <div className="p-8 flex items-center justify-center">
+      <canvas
+        id="canvas"
+        ref={canvasRef}
+        className="border border-solid border-sky-300"
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
+      ></canvas>
+    </div>
+  );
 };
 
 export default Workbench;
