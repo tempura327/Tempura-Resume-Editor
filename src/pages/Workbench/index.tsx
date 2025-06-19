5296bfd46e312d162cff6f454872817c122e9eaa
tempura327 <cks6658324@yahoo.com.tw>
2025-06-19 22:04:23 +0800

    feat: draw elements on canvas based on grid data

--- src/pages/Workbench/index.tsx
+++ src/pages/Workbench/index.tsx
@@ -1,15 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+import { drawShape, ShapeOptions } from '@/utils/index';
 
 const A4_RATIO = 1.38;
 const CANVAS_WIDTH = window.innerWidth * 0.66;
 const CANVAS_HEIGHT = CANVAS_WIDTH * A4_RATIO;
 
+const initialGridData: ShapeOptions[] = [];
+
 const Workbench = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const [gridData] = useState(initialGridData);
+
   useEffect(() => {
     const ctx = canvasRef.current?.getContext('2d');
-  }, []);
+
+    if (ctx) {
+      gridData.forEach((data) => {
+        drawShape(ctx, data);
+      });
+    }
+  }, [gridData]);
 
   return (
     <div className="p-8 flex items-center justify-center">
