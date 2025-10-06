302c56a741cd8906a8e37b2922fedda7465c5964
tempura327 <cks6658324@yahoo.com.tw>
2025-10-06 14:57:35 +0800

    feat: combined clsx and twMerge and got tlsx

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+
 export * from './check';
 export * from './draw';
 export * from './type';
+
+export const tlsx = (...params: (string | Record<string, boolean>)[]) => {
+  return twMerge(clsx(params));
+};
